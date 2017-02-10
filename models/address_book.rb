9ba0e74c9234d3bb1468b369df9b8626953bc52e
Meir Taffel <artgardening@gmail.com>
2017-02-10 11:12:22 +0000

    Complete checkpoint 5 assign. remove. add remove function to entries

--- models/address_book.rb
+++ models/address_book.rb
@@ -17,6 +17,14 @@ class AddressBook
         end
         entries.insert(index, Entry.new(name, phone_number, email))
     end
+    
+    def remove_entry(name, phone_number, email)
+        entries.each do |entry|
+            if name == entry.name
+                entries.delete(entry)
+            end
+        end
+    end
 end
     
     
